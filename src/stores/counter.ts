70f691fa773874c311e2808048c5e2cb6124f64f
coderHDY <986005715@qq.com>
2024-03-13 08:03:45 +0800

    test

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore("counter", () => {
 	const count = ref(0);
+
 	const doubleCount = computed(() => count.value * 2);
 	const increment = () => count.value++;
 
