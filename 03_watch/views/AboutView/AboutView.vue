54daf562608ddbf77b9051f46b9f6ecc9955bdfe
coderHDY <986005715@qq.com>
2024-05-10 16:17:14 +0800

    feat: watch

---
+++ 03_watch/views/AboutView/AboutView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+import { ref, watch, onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const nav = useRouter();
+
+
+const $counter = useCounterStore();
+
+</script>
+
+<template>
+	<div>
+		<div>{{ $counter.count }}</div>
+		<div class="transBtn">
+			<button @click="$counter.increment()">+</button>
+			<button @click="nav.push('/')">jump to home</button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.transBtn {
+	margin-top: 10px;
+	button {
+		margin-right: 10px;
+		border: 1px solid #000;
+		border-radius: 5px;
+		padding: 5px 10px;
+		cursor: pointer;
+		background-color: #dbe4dc;
+	}
+}
+</style>
