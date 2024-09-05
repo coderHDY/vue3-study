d7c1fb74c104b66659f4415b0ad174435ee45b0f
coderHDY <986005715@qq.com>
2024-09-05 14:45:15 +0800

    build

--- build_local.js
+++ build_local.js
@@ -4,5 +4,8 @@ const path = require("path");
 const distHtml = path.resolve(__dirname, "dist/index.html");
 const distHtmlContent = fs.readFileSync(distHtml, "utf8");
 // 将 type="module" crossorigin 替换为 defer
-const newDistHtmlContent = distHtmlContent.replace(/type="module" crossorigin/g, "defer");
+const newDistHtmlContent = distHtmlContent
+  .replace(/type="module" crossorigin/g, 'defer')
+  .replace(/"\//g, '"./')
+  .replace(/crossorigin/g, '');
 fs.writeFileSync(distHtml, newDistHtmlContent);
